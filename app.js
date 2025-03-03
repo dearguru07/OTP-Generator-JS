c149606273d2078f176d488d1c703ab0fbd5f982
dearguru07 <pangalaguruyadav101@gmail.com>
2025-03-03 16:18:34 +0530

    d

---
+++ app.js
@@ -0,0 +1,5 @@
+const arry=[]
+arry[0]='a'
+arry[1]='b'
+arry.length=0
+console.log(arry[0])
---
+++ app.py
@@ -0,0 +1,10 @@
+import pywhatkit
+from random2 import *
+
+phone_number='+919346860132'
+otp=randint(000000,999999)
+z=str(otp)
+msg='Ur OTP is ' + z + " Pls don't share any one"
+hour=8
+mints=10
+pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)
