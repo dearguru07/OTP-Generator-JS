da91dc762279903d31c4fab8cc1c5f77c3f64c58
dearguru07 <pangalaguruyadav101@gmail.com>
2025-03-04 20:42:22 +0530

    k

--- app.py
+++ app.py
@@ -8,3 +8,110 @@ msg='Ur OTP is ' + z + " Pls don't share any one"
 hour=8
 mints=10
 pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)
+
+
+
+
+
+
+
+import pywhatkit
+from random2 import *
+
+phone_number='+919346860132'
+otp=randint(000000,999999)
+z=str(otp)
+msg='Ur OTP is ' + z + " Pls don't share any one"
+hour=8
+mints=10
+pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)import pywhatkit
+from random2 import *
+
+phone_number='+919346860132'
+otp=randint(000000,999999)
+z=str(otp)
+msg='Ur OTP is ' + z + " Pls don't share any one"
+hour=8
+mints=10
+pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)import pywhatkit
+from random2 import *
+
+phone_number='+919346860132'
+otp=randint(000000,999999)
+z=str(otp)
+msg='Ur OTP is ' + z + " Pls don't share any one"
+hour=8
+mints=10
+pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)import pywhatkit
+from random2 import *
+
+phone_number='+919346860132'
+otp=randint(000000,999999)
+z=str(otp)
+msg='Ur OTP is ' + z + " Pls don't share any one"
+hour=8
+mints=10
+pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)import pywhatkit
+from random2 import *
+
+phone_number='+919346860132'
+otp=randint(000000,999999)
+z=str(otp)
+msg='Ur OTP is ' + z + " Pls don't share any one"
+hour=8
+mints=10
+pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)import pywhatkit
+from random2 import *
+
+phone_number='+919346860132'
+otp=randint(000000,999999)
+z=str(otp)
+msg='Ur OTP is ' + z + " Pls don't share any one"
+hour=8
+mints=10
+pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)import pywhatkit
+from random2 import *
+
+phone_number='+919346860132'
+otp=randint(000000,999999)
+z=str(otp)
+msg='Ur OTP is ' + z + " Pls don't share any one"
+hour=8
+mints=10
+pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)import pywhatkit
+from random2 import *
+
+phone_number='+919346860132'
+otp=randint(000000,999999)
+z=str(otp)
+msg='Ur OTP is ' + z + " Pls don't share any one"
+hour=8
+mints=10
+pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)import pywhatkit
+from random2 import *
+
+phone_number='+919346860132'
+otp=randint(000000,999999)
+z=str(otp)
+msg='Ur OTP is ' + z + " Pls don't share any one"
+hour=8
+mints=10
+pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)import pywhatkit
+from random2 import *
+
+phone_number='+919346860132'
+otp=randint(000000,999999)
+z=str(otp)
+msg='Ur OTP is ' + z + " Pls don't share any one"
+hour=8
+mints=10
+pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)import pywhatkit
+from random2 import *
+
+phone_number='+919346860132'
+otp=randint(000000,999999)
+z=str(otp)
+msg='Ur OTP is ' + z + " Pls don't share any one"
+hour=8
+mints=10
+pywhatkit.sendwhatmsg(phone_number,msg,hour,mints)
